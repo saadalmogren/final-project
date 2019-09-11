b56b866f916f393653aabacd22c864c14b8e118c
saad <saad-mogren@hotmail.com>
2019-09-11 17:05:03 +0300

    add comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
+
+  // Initialize AOS
   AOS.init({
     offset: 0,
-    duration: 500,
+    duration: 600,
     once: false
   })
+  //Home button 
   $("button").click(function () {
     $(".container").removeClass("exit").addClass("return")
     $(".team-info").attr("class", "team-info remove")
@@ -11,6 +14,7 @@ $(document).ready(function () {
 
 
   })
+  //animating after a league choosed
   $(".col-sm").click(function () {
     var img = $(this).children('img').attr('src')
     var id = $(this).attr('id');
@@ -19,7 +23,7 @@ $(document).ready(function () {
     $(".container").toggleClass("exit")
     $(".team-info").attr("class", "team-info enter")
     $('body').css('overflow-y', "auto")
-    
+    //gets standings for the selected league
     $.ajax({
       headers: { 'X-Auth-Token': 'a65dc5386ad14455a743b88dd047b5df' },
       url: `https://api.football-data.org/v2/competitions/${id}/standings`,
@@ -32,7 +36,7 @@ $(document).ready(function () {
       for (let i = 0; i < array.length; i++) {
         standings(array[i]);
       }
-
+      //appends standings positions
       function standings(pos) {
         var position = pos.position
         var name = pos.team.name
@@ -53,6 +57,7 @@ $(document).ready(function () {
 
       var matchDay = response.season.currentMatchday
       match(matchDay)
+      //gets matches of the selected league
       function match(matchDay) {
         $.ajax({
           headers: { 'X-Auth-Token': 'a65dc5386ad14455a743b88dd047b5df' },
@@ -60,9 +65,13 @@ $(document).ready(function () {
           dataType: 'json',
           type: 'GET',
         }).done(function (response) {
+          $("#list-home-list ,#list-profile-list").click(function () {
+            setTimeout(function () { AOS.refreshHard(); }, 300);
+          })
           var $team = $(`#matches-list`);
 
           for (let j = 0; j < response.matches.length; j++) {
+            //formats the date from the response
             var date = new Date(response.matches[j].utcDate);
             var month = date.getMonth() + 1;
             var day = date.getDate();
@@ -76,33 +85,33 @@ $(document).ready(function () {
             }
 
           }
-
+          // appends the finished matches
           function finish(match) {
             var home = match.homeTeam.name;
             var away = match.awayTeam.name;
             var homeScore = match.score.fullTime.homeTeam;
             var awayScore = match.score.fullTime.awayTeam;
 
-            $team.append(`<li class="list-group-item" id="list">
+            $team.append(`<li class="list-group-item" id="list" data-aos="fade-right" >
               <div class="date">${format}</div>
                 <div class = "match"><span class="home">${home}</span><span class="score"> ${homeScore + " - " + awayScore}</span><span class="away">${away}</span></div></li>`);
 
 
           }
-
+          //appends the scheduled matches
           function scheduled(match) {
             var home = match.homeTeam.name;
             var away = match.awayTeam.name;
 
             var hours = date.getHours();
             var min = date.getMinutes();
-
+            //adds zero to the miniutes less than 10 like 10:01 not 10:1
             if (min < 10) {
               min = "0" + min
             }
-            
+
             var time = hours + ":" + min;
-            $team.append(`<li class="list-group-item" id="list">
+            $team.append(`<li class="list-group-item" data-aos="fade-right"  id="list">
               <div class="date">${format}</div>
                 <div class = "match">
                 <span class="home">${home}</span><span class="time">${time}</span><span class="away">${away}</span>
@@ -110,6 +119,8 @@ $(document).ready(function () {
                 </li>`);
           }
         })
+
+
       }
     })
   })
